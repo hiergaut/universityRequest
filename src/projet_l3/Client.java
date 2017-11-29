21af68cf282a2f5f47959cd75f0ce16c3d539e3f
gauthier <gauthierbouyjou@aol.com>
2017-11-29 23:27:01 +0100

    add comment in client

--- src/projet_l3/Client.java
+++ src/projet_l3/Client.java
@@ -47,6 +47,7 @@ public class Client extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+	System.out.println("projet_l3.Client.mai()");
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
